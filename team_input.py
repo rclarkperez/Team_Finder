b12fce0c5b10aa5420811cb642eb10dc2757f85c
Ryan Clark <65691245+rclarkperez@users.noreply.github.com>
2023-03-01 22:33:53 -0800

    Add files via upload

---
+++ team_input.py
@@ -0,0 +1,28 @@
+import zmq
+
+
+context = zmq.Context()
+#  Socket to talk to server
+print("Connecting to spike server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(2):
+    if request == 0:
+        region_input = input("Please enter team city/region: ")
+        print(f"Sending request {region_input} …")
+        socket.send_string(region_input)
+
+        #  Get the reply.
+        message = socket.recv()
+        print(f"Received reply {region_input} [ {message} ]")
+    if request == 1:
+        team_name_input = input("Please enter team name: ")
+        print(f"Sending request {team_name_input} …")
+        socket.send_string(team_name_input)
+
+        #  Get the reply.
+        message = socket.recv()
+        print(f"Here is the link to the {region_input} {team_name_input} logo [ {message} ]")
+
